3065aa33e7a8b1d3f19a70c57b8ae5721567c7ea
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2017-09-04 22:27:40 +0200

    added animation with query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { animate, style, transition, trigger } from '@angular/animations';
+import {animate, group, query, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +8,52 @@ import { animate, style, transition, trigger } from '@angular/animations';
   animations: [
     trigger('panelState', [
       transition(':enter', [
-        style({
-          opacity: 0,
-          transform: 'translateX(100%)'
-        }),
-        animate(200)
+        group([
+          style({
+            opacity: 0,
+            transform: 'translateX(100%)'
+          }),
+          animate(200),
+
+          query(':self',[
+            style({transform:'translateY(-300px)', opacity:0}),
+            animate(300)
+          ]),
+          query('.panel-heading',[
+            style({transform:'translateY(-300px)', opacity:1}),
+            animate(300)
+          ]),
+          query('.panel-body',[
+            style({transform:'translateY(-100%)', opacity:1}),
+            animate(300)
+          ]),
+
+          query('.panel-footer',[
+            style({transform:'translateY(300px)', opacity:1}),
+            animate(300)
+          ]),
+        ]),
+
       ]),
       transition(':leave', [
         animate(200, style({
           transform: 'translateX(-100%)',
           opacity: 0
         }))
+      ]),
+      transition('* => *',[
+        query(':enter',[
+          style({transform:'scale(1)'}),
+          animate(200, style({transform:'scale(1.5)'})),
+          animate(100)
+        ],{optional:true}),
+        query('div > p, button',[
+          group([
+            animate(300, style({color:'red'})),
+            animate(300, style({transform:'scale(1.5)'}))
+          ])
+
+        ])
       ])
     ])
   ]
